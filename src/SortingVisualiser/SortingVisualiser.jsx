a71449ddc380dc5b51c360a0115712e5a3b13cd6
Linh0123 <gln8984@gmail.com>
2021-05-25 03:16:31 +0200

    Add function for disabling buttons whilst sorting

--- src/SortingVisualiser/SortingVisualiser.jsx
+++ src/SortingVisualiser/SortingVisualiser.jsx
@@ -22,8 +22,15 @@ export default class SortingVisualiser extends React.Component {
 
   resetArray(){
     const array = [];
+    var r = randomIntFromInterval(1, ARR_LENGTH);
+
     for (let i=0; i<ARR_LENGTH; i++){
       array.push(randomIntFromInterval(10,500));
+
+      if (i == r){
+        array.push(500);
+      }
+
     }
     this.setState({array});
   }
@@ -32,16 +39,20 @@ export default class SortingVisualiser extends React.Component {
 
   bubbleSort(){
     const animations = bubbleSortAnimation(this.state.array);
+    disable_buttons(animations.length, 1);
     animate(animations, 1);
   }
 
   quickSort(){
     const animations = quickSortAnimation(this.state.array)[1];
+    disable_buttons(animations.length, 50);
     animate(animations, 50);
   }
 
   mergeSort(){
     const animations = getMergeSortAnimations(this.state.array);
+    disable_buttons(animations.length, 5);
+
         for (let i = 0; i < animations.length; i++) {
           const arrayBars = document.getElementsByClassName('array-bar');
           const isColorChange = i % 3 !== 2;
@@ -66,6 +77,7 @@ export default class SortingVisualiser extends React.Component {
 
   heapSort(){
     const animations = heapSortAnimation(this.state.array);
+    disable_buttons(animations.length, 15);
     animate(animations, 15);
   }
 
@@ -87,11 +99,11 @@ export default class SortingVisualiser extends React.Component {
 
       ))}
         <div className='button-container'>
-          <button onClick={() => this.resetArray()}>New Array</button>
-          <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
-          <button onClick={() => this.quickSort()}>Quick Sort</button>
-          <button onClick={() => this.mergeSort()}>Merge Sort</button>
-          <button onClick={() => this.heapSort()}>Heap Sort</button>
+          <button class="controls" onClick={() => this.resetArray()}>New Array</button>
+          <button class="controls" onClick={() => this.bubbleSort()}>Bubble Sort</button>
+          <button class="controls" onClick={() => this.quickSort()}>Quick Sort</button>
+          <button class="controls" onClick={() => this.mergeSort()}>Merge Sort</button>
+          <button class="controls" onClick={() => this.heapSort()}>Heap Sort</button>
         </div>
 
       </div>
@@ -115,8 +127,30 @@ function animate(animations, speed){
         secondBarStyle.height = temp;
       }, i * speed)
     }
+
+
+
   }
 
 function randomIntFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function disable_buttons(len, speed)
+{
+  var x = document.getElementsByClassName("controls");
+
+  //Disable buttons
+  for(let i=0; i<x.length; i++)
+  {
+    x[i].disabled = true;
+  }
+
+  //Enable buttons once sorting is complete
+  setTimeout(() => {
+    for(let i=0; i<x.length; i++)
+    {
+      x[i].disabled = false;
+    }
+  }, len * speed)
 }
