89d7617098e83008a803436d5a8f35e8a3e07393
Linh0123 <gln8984@gmail.com>
2021-07-17 13:22:42 +0200

    Add more CSS styling

--- src/SortingVisualiser/SortingVisualiser.jsx
+++ src/SortingVisualiser/SortingVisualiser.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './SortingVisualiser.css';
 import {bubbleSortAnimation, quickSortAnimation, getMergeSortAnimations, heapSortAnimation} from '../sortingAlgorithms/sortingAlgorithms.js';
 
-const ARR_LENGTH = 120;
+const ARR_LENGTH = 60;
 
 export default class SortingVisualiser extends React.Component {
   constructor(props){
@@ -32,13 +32,13 @@ export default class SortingVisualiser extends React.Component {
   bubbleSort(){
     const animations = bubbleSortAnimation(this.state.array);
     disable_buttons();
-    animate(animations, 1);
+    animate(animations, 10);
   }
 
   quickSort(){
     const animations = quickSortAnimation(this.state.array)[1];
     disable_buttons();
-    animate(animations, 35);
+    animate(animations, 60);
   }
 
   async mergeSort(){
@@ -49,8 +49,11 @@ export default class SortingVisualiser extends React.Component {
       const arrayBars = document.getElementsByClassName('array-bar');
       const [barOneIdx, newHeight] = animations[i];
       const barStyle = arrayBars[barOneIdx].style;
+      barStyle.backgroundColor = '#FF522D';
       barStyle.height = `${newHeight}px`;
-      await new Promise(r => setTimeout(r, 1));
+      await new Promise(r => setTimeout(r, 10));
+
+      arrayBars[barOneIdx].style.backgroundColor = '#3888ff';
     }
     enable_buttons();
 }
@@ -58,7 +61,7 @@ export default class SortingVisualiser extends React.Component {
   heapSort(){
     const animations = heapSortAnimation(this.state.array);
     disable_buttons();
-    animate(animations, 8);
+    animate(animations, 15);
   }
 
 
@@ -75,6 +78,7 @@ export default class SortingVisualiser extends React.Component {
             height: `${value}px`,
             backgroundColor: '#3888ff'
           }}>
+          <span id={idx} class="hovertext">{value}</span>
         </div>
 
       ))}
@@ -98,10 +102,19 @@ async function animate(animations, speed){
     for(let i=0; i<animations.length; i++){
       const arrayBars = document.getElementsByClassName('array-bar');
       const [barOneIdx, barTwoIdx] = animations[i];
+      arrayBars[barOneIdx].style.backgroundColor = '#FF522D';
+      arrayBars[barTwoIdx].style.backgroundColor = '#FF522D';
+
       const temp = arrayBars[barOneIdx].style.height;
       arrayBars[barOneIdx].style.height = arrayBars[barTwoIdx].style.height;
+      document.getElementById(barOneIdx).textContent = arrayBars[barTwoIdx].style.height.slice(0, -2);
       arrayBars[barTwoIdx].style.height = temp;
+      document.getElementById(barTwoIdx).textContent = temp.slice(0, -2);
+
       await new Promise(r => setTimeout(r, speed));
+
+      arrayBars[barOneIdx].style.backgroundColor = '#3888ff';
+      arrayBars[barTwoIdx].style.backgroundColor = '#3888ff';
     }
     enable_buttons();
   }
@@ -124,9 +137,20 @@ function disable_buttons()
   }
 }
 
-//Renable button functionality
-function enable_buttons()
+//Renable button functionality and change bar colors to indicate completion of sorting
+async function enable_buttons()
 {
+  const arrayBars = document.getElementsByClassName('array-bar');
+  for(let i=0; i<arrayBars.length; i++){
+    arrayBars[i].style.backgroundColor = '#1abc9c';
+  }
+
+  await new Promise(r => setTimeout(r, 2000));
+
+  for(let i=0; i<arrayBars.length; i++){
+    arrayBars[i].style.backgroundColor = '#3888ff';
+  }
+
   var x = document.getElementsByClassName("controls-disabled");
 
   //Enable buttons once sorting is complete
